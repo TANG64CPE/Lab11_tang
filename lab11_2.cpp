6939e4b1bd2ad43eba250782607176c115394c87
TANGCPE64 <natsuphat_th@cmu.ac.th>
2021-12-17 04:40:16 +0700

    Completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -11,7 +11,17 @@ int main (){
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	dest << "-------------------- BOOM ---------------------" << endl;
+	dest << "AR CHI" << endl;
+	dest << "AR BOW" << endl;
+	dest << "CHOW CHOW" << endl;
+	dest << "BA KA" << endl;
+	dest << "BA GEAR" << endl;
+	dest << "CHEER CHEER" << endl;
+	dest << "WE ARE ALL" << endl;
+	dest << "ENTANEER" << endl;
+	dest << "CMU" << endl;
+	dest << "-------------------- HA!! ---------------------" << endl;
     source.close();
     dest.close();
 	return 0;
